64aa916ac42274b2154ebe6577ca0d84fbf05883
Ewan <mathesonewan@gmail.com>
2025-05-18 19:27:26 +0100

    Moved Config to env0.terminal

---
+++ src/env0.terminal/Config/ConfigManager.cs
@@ -0,0 +1,10 @@
+namespace env0.terminal
+{
+    public static class ConfigManager
+    {
+        public static string BootConfigPath => "../Boot/BootConfig.json";
+        public static string FilesystemsPath => "../Filesystem/Files/";
+        public static string DeviceConfigPath => "../Network/Devices.json";
+        public static string UserConfigPath => "../Login/UserConfig.json";
+    }
+}
